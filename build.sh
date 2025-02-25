18300c3082c83a7bb8ec579daa9f6ccd47e83351
BernardoSeijasCavalcante <bernardomseijas@gmail.com>
2025-02-25 18:12:15 -0300

    Atualização no build.sh para versões Ubuntu Render

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-# Atualiza os pacotes e instala dependências
+# Atualiza os pacotes e instala dependências do ODBC
 apt-get update && apt-get install -y curl gnupg2
 
 # Adiciona a chave do repositório da Microsoft
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-# Adiciona o repositório do SQL Server ODBC Driver
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+# Detecta a versão do Ubuntu
+UBUNTU_VERSION=$(lsb_release -rs)
+
+# Adiciona o repositório do SQL Server ODBC Driver baseado na versão do Ubuntu
+if [ "$UBUNTU_VERSION" == "22.04" ]; then
+    curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+else
+    curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+fi
 
 # Atualiza novamente os pacotes
 apt-get update
 
 # Instala o driver ODBC 18 para SQL Server e o gerenciador unixODBC
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 unixodbc-dev
+
+# Verifica se o driver foi instalado corretamente
+odbcinst -q -d
 
 # Instala as dependências do Python
 pip install --upgrade pip
